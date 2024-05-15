83853544f8dfa5a8be600a090549da010c95c217
genius <girls0330@naver.com>
2024-05-15 22:54:58 +0900

    Initial commit

---
+++ src/main/java/com/job/dashboard/JobDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.job.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/job/dashboard/JobDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
